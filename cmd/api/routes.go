d932bae76ab784e6ad48c4281083c4eb23a3179c
abner-tech <2020151792@ub.edu.bz>
2024-10-25 15:39:13 -0600

    adding video

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -18,10 +18,10 @@ func (a *applicationDependences) routes() http.Handler {
 
 	//setup routes
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", a.healthChechHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/comments", a.createCommentHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/comments/:id", a.displayCommentHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/comments/:id", a.updateCommentHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/comments/:id", a.deleteCommentHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/comments", a.listCommentHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/signin", a.createCommentHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/signin/:id", a.displayCommentHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/signin/:id", a.updateCommentHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/signin/:id", a.deleteCommentHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/signin", a.listCommentHandler)
 	return a.recoverPanic(router)
 }
